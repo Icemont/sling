b4a24f924996522482e99e0a2922ed8a101ae17b
Icemont <ray.icemont@gmail.com>
2022-08-28 16:02:44 +0400

    Main font updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,15 @@
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('assets/js/app.js') }}"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <style>
+        :root {
+            --tblr-font-sans-serif: 'Inter',-apple-system,BlinkMacSystemFont,San Francisco,Segoe UI,Roboto,Helvetica Neue,sans-serif !important;
+        }
+    </style>
 </head>
 <body class="theme-light">
 @include('layouts.navigation')
