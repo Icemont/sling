b32837d23641632dc7741589c39d654e0b2d810f
Icemont <ray.icemont@gmail.com>
2022-10-05 00:59:43 +0400

    Routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\Auth\UpdatePasswordController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DashboardController;
@@ -9,49 +11,38 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserSettingsController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified', 'filled'])->group(function () {
-    Route::get('/user/password', [UpdatePasswordController::class, 'edit'])
-        ->name('user.password.edit');
-    Route::put('/user/password', [UpdatePasswordController::class, 'update'])
-        ->name('user.password.update');
-
-    Route::middleware(['password.confirm'])->group(function () {
-        Route::get('/user/settings', [UserSettingsController::class, 'edit'])
-            ->name('user.settings.edit');
-        Route::put('/user/settings', [UserSettingsController::class, 'update'])
-            ->name('user.settings.update');
-    });
-
+Route::middleware(['auth', 'verified', 'filled'])->group(static function () {
     Route::get('/', DashboardController::class)->name('dashboard');
 
     Route::resource('clients', ClientController::class)->except(['create']);
-
     Route::resource('payment-methods', PaymentMethodController::class);
-
     Route::resource('invoices', InvoiceController::class)->except(['create']);
-    Route::post('/invoices/create', [InvoiceController::class, 'createForm'])->name('invoices.createform');
-    Route::get('/invoices/create/{client}', [InvoiceController::class, 'create'])
-        ->where('client', '[0-9]+')
-        ->name('invoices.create');
-    Route::get('/invoices/download/{invoice}', [InvoiceController::class, 'download'])
-        ->where('invoice', '[0-9]+')
-        ->name('invoices.download');
+
+    Route::prefix('invoices')->name('invoices.')->group(static function () {
+        Route::post('/create', [InvoiceController::class, 'createForm'])->name('createform');
+
+        Route::get('/create/{client}', [InvoiceController::class, 'create'])
+            ->whereNumber('client')
+            ->name('create');
+
+        Route::get('/download/{invoice}', [InvoiceController::class, 'download'])
+            ->whereNumber('invoice')
+            ->name('download');
+    });
 
     Route::get('/reports', [ReportController::class, 'form'])->name('reports.form');
     Route::post('/reports', [ReportController::class, 'create'])->name('reports.create');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/user/password', [UpdatePasswordController::class, 'edit'])
-        ->name('user.password.edit');
-    Route::put('/user/password', [UpdatePasswordController::class, 'update'])
-        ->name('user.password.update');
+Route::middleware(['auth', 'verified'])->group(static function () {
+    Route::prefix('user')->name('user.')->group(static function () {
+        Route::get('/password', [UpdatePasswordController::class, 'edit'])->name('password.edit');
+        Route::put('/password', [UpdatePasswordController::class, 'update'])->name('password.update');
 
-    Route::middleware(['password.confirm'])->group(function () {
-        Route::get('/user/settings', [UserSettingsController::class, 'edit'])
-            ->name('user.settings.edit');
-        Route::put('/user/settings', [UserSettingsController::class, 'update'])
-            ->name('user.settings.update');
+        Route::middleware(['password.confirm'])->group(static function () {
+            Route::get('/settings', [UserSettingsController::class, 'edit'])->name('settings.edit');
+            Route::put('/settings', [UserSettingsController::class, 'update'])->name('settings.update');
+        });
     });
 });
 
